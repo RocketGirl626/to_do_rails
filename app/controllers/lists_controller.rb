52d0b617700138528cf61e855506465fc7011147
Jeanette Fairless <jfairless@spectrumhomeservices.com>
2015-06-29 10:41:42 -0700

    complete to the end of weekend homework

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,7 +14,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to list_path
+      redirect_to lists_path
     else
       render :new
     end
@@ -22,6 +22,25 @@ class ListsController < ApplicationController
   def edit
     @lists = List.find(params[:id])
     render :edit
+  end
+  def edit
+    @list = List.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
   end
 
   private
